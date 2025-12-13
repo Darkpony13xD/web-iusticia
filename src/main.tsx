6bc9af5ed8cd015a56740fb57c023f7bf0b033cb
Darkpony13xD <darkpony13xd@gmail.com>
2025-12-12 23:04:28 -0600

    favicon y manifest

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
